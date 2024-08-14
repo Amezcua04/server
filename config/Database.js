ec57f47958af3ebfd4290c21f2c2ed47321e65a1
CarlosAmezcua <Amezcua041196@gmail.com>
2024-08-14 16:36:00 -0700

    FIXED: variables de entorno

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,15 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
-const db = new Sequelize("server_db", "root", "root", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.HOST,
+    dialect: "mysql",
+  }
+);
 
 export default db;
--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const store = new sessionStore({
 
 app.use(
   session({
-    secret: process.env.SESSION_SECRET,
+    secret: "Atenea07",
     resave: false,
     saveUninitialized: false,
     store: store,
